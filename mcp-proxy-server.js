4ef26843437fa2c6e3b092e69c8aebf99a757270
Shubham Pawar <shubhammukundpawar@gmail.com>
2026-08-27 00:16:02 +0530

    UI link added

--- mcp-proxy-server.js
+++ mcp-proxy-server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 // app.use(cors());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://shubhampawar13.github.io');
     res.header(
         'Access-Control-Allow-Methods',
         'GET,POST,PUT,DELETE,OPTIONS'
