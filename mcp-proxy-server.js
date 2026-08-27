a8c0f7cafc63b8d2c98199b80184cfad9d59d79b
Shubham Pawar <shubhammukundpawar@gmail.com>
2026-08-27 21:05:04 +0530

    OPTIon method handler added

--- mcp-proxy-server.js
+++ mcp-proxy-server.js
@@ -9,15 +9,24 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 // app.use(cors());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
     res.header(
-        'Access-Control-Allow-Methods',
-        'GET,POST,PUT,DELETE,OPTIONS'
+        "Access-Control-Allow-Origin",
+        "https://shubhampawar13.github.io"
     );
+
     res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        "Access-Control-Allow-Methods",
+        "GET,POST,PUT,DELETE,OPTIONS"
     );
+
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(204);
+    }
 
     next();
 });
